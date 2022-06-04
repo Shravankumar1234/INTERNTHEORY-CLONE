4b49b5a95a54464773080e54906c989810e17c71
nimishshinde <mayurshinde2310@gmail.com>
2022-06-04 19:31:43 +0530

    updated

---
+++ Components/footer.js
@@ -0,0 +1,46 @@
+const FooterFunction=()=>{
+    return `<div>
+    <div>
+      <div onclick="window.location.href='https://www.facebook.com/interntheory'"><i class="fab fa-facebook-square"></i></div>
+      <div onclick="window.location.href='https://www.linkedin.com/company/interntheory/about/'"><i class="fab fa-linkedin"></i></div>
+      <div onclick="window.location.href='https://twitter.com/InternTheory'"><i class="fab fa-twitter-square"></i></div>
+      <div onclick="window.location.href='https://www.instagram.com/interntheory/'"><i class="fab fa-instagram-square"></i></div>
+    </div>
+    <div>
+      <div>
+        <div class="FooterThreeHeading">BY PLACE</div>
+        <div class="FooterList">Internships In Mumbai</div>
+        <div class="FooterList">Internships In Delhi</div>
+        <div class="FooterList">Internships In Bangalore</div>
+        <div class="FooterList">Internships In Pune</div>
+        <div class="FooterList">Internships In Hyderabad</div>
+      </div>
+      <div>
+        <div class="FooterThreeHeading">BY PROFILE</div>
+        <div class="FooterList">Marketing Internships</div>
+        <div class="FooterList">Business Development Internships</div>
+        <div class="FooterList">Content Writing Internships</div>
+        <div class="FooterList">Graphic Designing Internships</div>
+        <div class="FooterList">HR Internships</div>
+        <div class="FooterList">Engineering Internships</div>
+      </div>
+      <div>
+        <div class="FooterThreeHeading">BY TYPE</div>
+        <div class="FooterList">Full Time Internships</div>
+        <div class="FooterList">Part Time Internships</div>
+        <div class="FooterList">Work From Home Internships</div>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div>REFUND & CANCELLATION POLICY</div>
+    <div>COURSES</div>
+    <div>TERMS AND CONDITIONS</div>
+    <div>PRIVACY POLICY</div>
+    <div>CONTACT US</div>
+    <div>ABOUT US</div>
+    <div>SITEMAP</div>
+  </div>
+  <div>© 2015 - 2022 INTERNTHEORY. ALL RIGHTS RESERVED.</div>`
+}
+export{FooterFunction}
